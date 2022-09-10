22cb5632e473254629931b26e6e5014030532dcd
AadarshDubey <addydubey321@gmail.com>
2022-09-10 15:21:35 +0530

    updated

--- server/index.js
+++ server/index.js
@@ -10,12 +10,10 @@ app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
 
-const CONNECTION_URL = 'mongodb+srv://Aady:BLOG@11@cluster0.p1lydzt.mongodb.net/?retryWrites=true&w=majority';
+const CONNECTION_URL = 'mongodb+srv://Aady:BLOG%4011@cluster0.p1lydzt.mongodb.net/?retryWrites=true&w=majority';
 
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => app.listen(PORT, () =>console.log(`server running on port: ${PORT}` )))
-  .catch((error) => console.log(error.message));
- 
- mongoose.set('useFindAndModify', false);
+mongoose.connect(CONNECTION_URL)
+.then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+.catch((err) => console.log(err.message));
